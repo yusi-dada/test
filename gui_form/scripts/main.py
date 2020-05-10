c908deab28aa6140afe2b0505c3f0035c00616b4
yusi-dada <63641224+yusi-dada@users.noreply.github.com>
2020-05-11 01:02:22 +0900

    Update main.py

--- gui_form/scripts/main.py
+++ gui_form/scripts/main.py
@@ -1,1 +1,41 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# ********************************
+#
+#	 library
+#
+# ********************************
+import rospy
+import os
+import sys
+import signal
 
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+
+from form.gui_manager import *
+# ********************************
+#
+#	 signal
+#
+# ********************************
+def sigint_handler(*args):
+  QApplication.quit()
+# ********************************
+#
+#	 main
+#
+# ********************************
+if __name__ == '__main__':
+  rospy.init_node('gui_form', anonymous=True) 
+  signal.signal(signal.SIGINT, sigint_handler)
+
+  print(sys.version)
+
+  app = QApplication(sys.argv)
+  
+  window = gui_manager()
+  window.show()
+  window.setTimer(100)    # timer setting
+  
+  sys.exit(app.exec_())
